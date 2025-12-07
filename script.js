d682be4e5654ed7c0d02a8df6c105fe875e85b0e
Godwin <godwinokon112@gmail.com>
2025-12-07 15:50:28 +0100

    just finish building- first push

---
+++ script.js
@@ -0,0 +1,18 @@
+function checkAnswer() {
+  let correctAnswer = "4";
+  let userAnswer = document.querySelector("input[name='quiz']:checked").value;
+  let feedback = document.getElementById("feedback");
+
+  if (userAnswer === correctAnswer) {
+    feedback.textContent = "Correct! Well done.";
+    feedback.style.color = "green";
+    console.log(yes);
+    
+  } else {
+    feedback.textContent = "that's Incorrect. Please try again.";
+    feedback.style.color = "red";
+  }
+}
+let submitButton = document.getElementById("submit-answer");
+
+submitButton.addEventListener("submit", checkAnswer);
